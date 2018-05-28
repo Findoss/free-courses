778a931189bd51e982a536c6a41834eecf997cb6
Nikita Stroganov <findoss@yandex.ru>
2018-05-29 02:06:31 +0300

    solved the task 1

--- task1.js
+++ task1.js
@@ -1,3 +1,18 @@
 /* Task 1 */
+/* Реализовать функцию, исключающую из входного строки русские буквы Р, К и Н в любом регистре.
+На вход подается строка длины n. 1 <= n <= 10000 */
 
-/* Put your code here */
+/**
+ * Функция filter() сключает из входной строки заданные символы в любом регистре.
+ * @param {String} string Фильтруемая строка
+ * @param {...char} filterChars Символы, используемые в качестве правил фильтра. 
+ * @returns {String} Возвращает отфильтрованную строку
+ */
+function filter(string, ...filterChars) {
+  const excluding = filterChars.reduce((previousValue, currentValue) =>  {
+    return `${previousValue}|${currentValue}`;
+  });
+  return string.replace(new RegExp(excluding, 'gi'),'');
+};
+
+process.stdout.write(filter(process.argv[process.argv.length-1], 'Р', 'К', 'Н'));
